9786250069789cbcabfa25324e4c3b7e5a807ee3
laramai13 <lara.mai13@gmail.com>
2026-05-10 21:03:34 +0200

    fixed line sorting

--- project.py
+++ project.py
@@ -6,13 +6,20 @@ import os
 import time
 
 
-
 def callback(input):
     pass
 
-def cannyEdge():
+def x_intersect(rho, theta):
+    # Schnittpunkt mit y = 0
+    return rho / np.cos(theta)
+
+def y_intersect(rho, theta):
+    # Schnittpunkt mit x = 0
+    return rho / np.sin(theta)
+
+def cornerDetection():
     root = os.getcwd()
-    imgPath = os.path.join(root, "images\\test1.jpeg")
+    imgPath = os.path.join(root, "images\\test2.jpeg")
     og_img = cv.imread(imgPath)
 
     img = cv.cvtColor(og_img,cv.COLOR_BGR2RGB)
@@ -37,13 +44,14 @@ def cannyEdge():
     lines = cv.HoughLines(cannyEdge, 1, np.pi/180,80)
     
     paperEdges = []
-    col = [(0,0,255),(0,255,0),(255,0,0), (255,0,255),(255,0,255),(0,255,255),(255,255,255),(0,0,0),(100,100,100)]
+    col = [(0,0,0),(0,255,0),(255,0,0),(255,0,255)]
     horizontal = []
     vertical = []
+    verticalAbsRho = []
+    horizontalAbsRho = [] 
     if lines is not None:
         for i in range(0, len(lines)):
             rho = lines[i][0][0]
-            print(col[i])
             print("Rho: " + str(rho))
             theta = lines[i][0][1]
             print("Theta: " + str(theta))
@@ -55,25 +63,27 @@ def cannyEdge():
             pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
             #cv.line(overlay, pt1, pt2, col[i], 3, cv.LINE_AA)
 
+            absRho = rho
             if theta > np.pi/2.0:
-                rho = abs(rho)
-                lines[i][0][0] = rho
+                absRho = abs(rho)
+            #    lines[i][0][0] = rho
     
             if(((theta <= np.pi*0.25 and theta >=0)  or (theta > np.pi*1.75 )) or (theta > np.pi*0.75 and theta <= np.pi*1.25)):
                 print("vertical")
                 vertical.append(lines[i])
+                verticalAbsRho.append(absRho)
                 #cv.line(overlay, pt1, pt2, (255,0,0), 3, cv.LINE_AA)
             else:
                 print("horizontal")
                 horizontal.append(lines[i])
+                horizontalAbsRho.append(absRho)
                 #cv.line(overlay, pt1, pt2, (0,255,0), 3, cv.LINE_AA)
     print(len(vertical), len(horizontal))
 
-    print("start kmeans")
-    verticalRho = np.array([vert[0][0] for vert in vertical], dtype=np.float32).reshape(-1, 1)
-    horizontalRho = np.array([hor[0][0] for hor in horizontal], dtype=np.float32).reshape(-1, 1)
-    
+    verticalRho = np.array(verticalAbsRho, dtype=np.float32).reshape(-1, 1)
+    horizontalRho = np.array(horizontalAbsRho, dtype=np.float32).reshape(-1, 1)
 
+    print(verticalRho)
     vertLabels = KMeans(n_clusters=2, n_init=1).fit_predict(verticalRho)
     print(vertLabels)
     horLabels = KMeans(n_clusters=2, n_init=1).fit_predict(horizontalRho)
@@ -109,39 +119,66 @@ def cannyEdge():
             if(abs(meanrho - cat[j][0][0]) < delta):
                 delta = abs(meanrho - cat[j][0][0])
                 bestIdx = j
+
+        bestLine = cat[bestIdx]
+
+        rho = bestLine[0][0]
+        theta = bestLine[0][1]
         #sort into bestLines[]
         if(i < 2):
             #vertical
+            xi = x_intersect(rho, theta)
+
             if bestLines[0] is None:
-                bestLines[0] = cat[bestIdx]
+                bestLines[0] = bestLine
             else:
-                if(bestLines[0][0][0] > cat[bestIdx][0][0]):
-                    bestLines[1]= bestLines[0]
-                    bestLines[0] = cat[bestIdx]
+                # Vergleiche x-Schnittpunkte
+                rho0 = bestLines[0][0][0]
+                theta0 = bestLines[0][0][1]
+                xi0 = x_intersect(rho0, theta0)
+
+                if xi < xi0:
+                    bestLines[1] = bestLines[0]
+                    bestLines[0] = bestLine
                 else:
-                    bestLines[1] = cat[bestIdx]
+                    bestLines[1] = bestLine
         else:
             #horizontal
+            yi = y_intersect(rho, theta)
+
             if bestLines[2] is None:
-                bestLines[2] = cat[bestIdx]
+                bestLines[2] = bestLine
             else:
-                if(bestLines[2][0][0] > cat[bestIdx][0][0]):
-                    bestLines[3]= bestLines[2]
-                    bestLines[2] = cat[bestIdx]
+                rho2 = bestLines[2][0][0]
+                theta2 = bestLines[2][0][1]
+                yi0 = y_intersect(rho2, theta2)
+
+                if yi < yi0:
+                    bestLines[3] = bestLines[2]
+                    bestLines[2] = bestLine
                 else:
-                    bestLines[3] = cat[bestIdx]
+                    bestLines[3] = bestLine
 
     print(bestLines)
     for i in range(len(bestLines)):
         rho = bestLines[i][0][0]
         theta = bestLines[i][0][1]
+        print(rho)
+        print(theta)
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
         y0 = b * rho
         pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
         pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-        cv.line(overlay, pt1, pt2, col[i], 3, cv.LINE_AA)
+        print(pt1)
+        print(pt2)
+        #cv.line(overlay, pt1, pt2, col[i], 3, cv.LINE_AA)
+        h, w = overlay.shape[:2]
+        ok, clipped_pt1, clipped_pt2 = cv.clipLine((0, 0, w, h), pt1, pt2)
+
+        if ok:
+            cv.line(overlay, clipped_pt1, clipped_pt2, col[i], 3, cv.LINE_AA)
 
    
     contours, hierarchy = cv.findContours(cannyEdge, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -163,4 +200,4 @@ if __name__ == "__main__":
     dummy = np.array([[0],[1]], dtype=np.float32)
     KMeans(n_clusters=2, n_init=1).fit_predict(dummy)
 
-    cannyEdge()
+    cornerDetection()
