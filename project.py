0278dfac1a825a2eee1d44782c83903a8ab53986
laramai13 <lara.mai13@gmail.com>
2026-05-12 15:41:46 +0200

    video detection with droid cam

--- project.py
+++ project.py
@@ -9,6 +9,35 @@ import time
 def callback(input):
     pass
 
+def cameraFeed():
+    cap = cv.VideoCapture("http://172.18.39.237:4747/video")
+    bestLines = [None,None,None,None]
+    count = 0
+    while True:
+        cap.set(cv.CAP_PROP_BUFFERSIZE, 1)
+        ret, frame = cap.read()
+        cap.grab()
+        if not ret:
+            continue
+        count += 1
+        h, w = frame.shape[:2]
+        crop_height = 80
+        frame = frame[crop_height:h,0:w]
+        if(count %3 != 0):
+            cv.waitKey(1)
+            continue
+        og_img = frame.copy()
+        overlay,bestLines = cornerDetection(og_img,bestLines)
+        if overlay is None: continue
+
+        cv.imshow("DroidCam Feed", overlay)
+
+        if cv.waitKey(1) == 27:  # ESC
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
 def x_intersect(rho, theta):
     # Schnittpunkt mit y = 0
     return rho / np.cos(theta)
@@ -17,31 +46,38 @@ def y_intersect(rho, theta):
     # Schnittpunkt mit x = 0
     return rho / np.sin(theta)
 
-def cornerDetection():
-    root = os.getcwd()
-    imgPath = os.path.join(root, "images\\test2.jpeg")
-    og_img = cv.imread(imgPath)
 
+
+def cornerDetection(og_img, lastBestLines):
+    #root = os.getcwd()
+    #imgPath = os.path.join(root, "images\\test2.jpeg")
+    #og_img = cv.imread(imgPath)
+    
     img = cv.cvtColor(og_img,cv.COLOR_BGR2RGB)
 
     height,width,_ = img.shape
     scale = 1/5
     heightScale = int(height*scale)
     widthScale = int(width*scale)
-    img = cv.resize(img,(widthScale,heightScale), interpolation=cv.INTER_LINEAR)
-    og_img = cv.resize(img,(widthScale,heightScale), interpolation=cv.INTER_LINEAR)
+
+    
+
+    #img = cv.resize(img,(widthScale,heightScale), interpolation=cv.INTER_LINEAR)
+    #og_img = cv.resize(img,(widthScale,heightScale), interpolation=cv.INTER_LINEAR)
     overlay = og_img.copy()
-    name = "canny"
-    cv.namedWindow(name)
+    #name = "canny"
+    #cv.namedWindow(name)
 
     #cv.createTrackbar("minThresh",name,0,255,callback)
     #cv.createTrackbar("maxThresh",name,0,255,callback)
 
-    minThresh = 150
-    maxThresh = 250
+    minThresh = 30
+    maxThresh = 90
     
-    cannyEdge = cv.Canny(img, minThresh,maxThresh)
-    lines = cv.HoughLines(cannyEdge, 1, np.pi/180,80)
+    blur = cv.GaussianBlur(img, (5,5), 0)
+    cannyEdge = cv.Canny(blur, minThresh,maxThresh)
+    edges = cv.dilate(cannyEdge, None, iterations=1)
+    lines = cv.HoughLines(edges, 1, np.pi/180,80)
     
     paperEdges = []
     col = [(0,0,0),(0,255,0),(255,0,0),(255,0,255)]
@@ -52,9 +88,9 @@ def cornerDetection():
     if lines is not None:
         for i in range(0, len(lines)):
             rho = lines[i][0][0]
-            print("Rho: " + str(rho))
+            #print("Rho: " + str(rho))
             theta = lines[i][0][1]
-            print("Theta: " + str(theta))
+            #print("Theta: " + str(theta))
             a = np.cos(theta)
             b = np.sin(theta)
             x0 = a * rho
@@ -69,25 +105,30 @@ def cornerDetection():
             #    lines[i][0][0] = rho
     
             if(((theta <= np.pi*0.25 and theta >=0)  or (theta > np.pi*1.75 )) or (theta > np.pi*0.75 and theta <= np.pi*1.25)):
-                print("vertical")
+                #print("vertical")
                 vertical.append(lines[i])
                 verticalAbsRho.append(absRho)
                 #cv.line(overlay, pt1, pt2, (255,0,0), 3, cv.LINE_AA)
             else:
-                print("horizontal")
+                #print("horizontal")
                 horizontal.append(lines[i])
                 horizontalAbsRho.append(absRho)
                 #cv.line(overlay, pt1, pt2, (0,255,0), 3, cv.LINE_AA)
-    print(len(vertical), len(horizontal))
+    #print(len(vertical), len(horizontal))
+
+    if(len(vertical) < 2 or len(horizontal) < 2): return overlay, lastBestLines
 
     verticalRho = np.array(verticalAbsRho, dtype=np.float32).reshape(-1, 1)
     horizontalRho = np.array(horizontalAbsRho, dtype=np.float32).reshape(-1, 1)
-
-    print(verticalRho)
-    vertLabels = KMeans(n_clusters=2, n_init=1).fit_predict(verticalRho)
-    print(vertLabels)
-    horLabels = KMeans(n_clusters=2, n_init=1).fit_predict(horizontalRho)
-    print(horLabels)
+    if(len(verticalRho) ==0 or len(horizontalRho)== 0):
+        return overlay, lastBestLines
+    
+    try:
+        vertLabels = KMeans(n_clusters=2, n_init=1).fit_predict(verticalRho)
+        #print(vertLabels)
+        horLabels = KMeans(n_clusters=2, n_init=1).fit_predict(horizontalRho)
+        #print(horLabels)
+    except: return overlay, lastBestLines
 
     categorizedLines = [[],[],[],[]]
 
@@ -159,20 +200,26 @@ def cornerDetection():
                 else:
                     bestLines[3] = bestLine
 
-    print(bestLines)
+    #print(bestLines)
+
+    if(lastBestLines is not None):
+        for i in range(len(bestLines)):
+            if(bestLines[i] is None and lastBestLines[i] is not None):
+                bestLines[i] = lastBestLines[i]
+
     for i in range(len(bestLines)):
         rho = bestLines[i][0][0]
         theta = bestLines[i][0][1]
-        print(rho)
-        print(theta)
+        #print(rho)
+        #print(theta)
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
         y0 = b * rho
         pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
         pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-        print(pt1)
-        print(pt2)
+        #print(pt1)
+        #print(pt2)
         #cv.line(overlay, pt1, pt2, col[i], 3, cv.LINE_AA)
         h, w = overlay.shape[:2]
         ok, clipped_pt1, clipped_pt2 = cv.clipLine((0, 0, w, h), pt1, pt2)
@@ -181,9 +228,9 @@ def cornerDetection():
             cv.line(overlay, clipped_pt1, clipped_pt2, col[i], 3, cv.LINE_AA)
 
    
-    contours, hierarchy = cv.findContours(cannyEdge, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    #contours, hierarchy = cv.findContours(cannyEdge, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    
+    return overlay,bestLines
     while True:
         if cv.waitKey(1) == ord("q"):
             break
@@ -199,5 +246,5 @@ if __name__ == "__main__":
     #KMeans warm up
     dummy = np.array([[0],[1]], dtype=np.float32)
     KMeans(n_clusters=2, n_init=1).fit_predict(dummy)
-
-    cornerDetection()
+    cameraFeed()
+    #cornerDetection()
